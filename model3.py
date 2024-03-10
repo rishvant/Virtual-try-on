4f2b4660786b88056e4da9fba919749adaad104c
Rishvant Singh <rishvantsinghpundir@gmail.com>
2024-03-10 10:40:45 +0530

    add model

---
+++ model3.py
@@ -0,0 +1,61 @@
+import os
+import cv2
+from cvzone.PoseModule import PoseDetector
+
+cap = cv2.VideoCapture(0)
+detector = PoseDetector()
+
+shirtFolderPath = r"./"
+listShirts = os.listdir(shirtFolderPath)
+fixedRatio = 262 / 190  # widthOfShirt/widthOfPoint11to12
+shirtRatioHeightWidth = 581 / 440
+imageNumber = 0
+imgButtonRight = cv2.imread(r"./", cv2.IMREAD_UNCHANGED)
+imgButtonLeft = cv2.flip(imgButtonRight, 1)
+counterRight = 0
+counterLeft = 0
+selectionSpeed = 10
+
+while True:
+    success, img = cap.read()
+    img = detector.findPose(img)
+    lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False, draw=False)
+    if lmList:
+        lm11 = lmList[11][1:3]
+        lm12 = lmList[12][1:3]
+        imgShirt = cv2.imread(os.path.join(shirtFolderPath, listShirts[imageNumber]), cv2.IMREAD_UNCHANGED)
+
+        widthOfShirt = int((lm11[0] - lm12[0]) * fixedRatio)
+        heightOfShirt = int(widthOfShirt * shirtRatioHeightWidth)
+        if widthOfShirt > 0 and heightOfShirt > 0:  # Check if width and height are valid
+            imgShirt = cv2.resize(imgShirt, (widthOfShirt, heightOfShirt))
+            currentScale = (lm11[0] - lm12[0]) / 190
+            offset = int(44 * currentScale), int(48 * currentScale)
+
+            try:
+                img = cvzone.overlayPNG(img, imgShirt, (lm12[0] - offset[0], lm12[1] - offset[1]))
+            except:
+                pass
+
+        if lmList[16][1] < 300:
+            counterRight += 1
+            cv2.ellipse(img, (139, 360), (66, 66), 0, 0,
+                        counterRight * selectionSpeed, (0, 255, 0), 20)
+            if counterRight * selectionSpeed > 360:
+                counterRight = 0
+                if imageNumber < len(listShirts) - 1:
+                    imageNumber += 1
+        elif lmList[15][1] > 900:
+            counterLeft += 1
+            cv2.ellipse(img, (1138, 360), (66, 66), 0, 0,
+                        counterLeft * selectionSpeed, (0, 255, 0), 20)
+            if counterLeft * selectionSpeed > 360:
+                counterLeft = 0
+                if imageNumber > 0:
+                    imageNumber -= 1
+        else:
+            counterRight = 0
+            counterLeft = 0
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
